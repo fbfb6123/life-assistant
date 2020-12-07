6bf1eacc6a91a19210d31ade68b80af19ae52b73
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-12-07 22:40:00 +0900

    Update MyComponent.js

--- resources/js/components/MyComponent.js
+++ resources/js/components/MyComponent.js
@@ -1,2 +1,15 @@
 import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
+
+export default class MyComponent extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      num:0,
+      msg:'OK',
+    };
+    this.doChange = this.doChange.bind(this);
+  }
+
+  
+}
