a5679dfc2a6415384547277de146bc61ce6bd752
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2020-12-07 22:28:13 +0900

    Update index.blade.php

--- resources/views/hello/index.blade.php
+++ resources/views/hello/index.blade.php
@@ -1,1 +1,17 @@
-test
+<!doctype html>
+<html lang="ja">
+<head>
+<title>Index</title>
+<link href="{{ mix('css/app.css') }}"
+      rel="stylesheet" type="text/css">
+<meta name="csrf-token" content="{{ csrf_token() }}">
+</head>
+<body style="padding:10px;">
+      <h1>Hello</h1>
+      <p>{{$msg}}</p>
+
+      <div id="example"></div>
+
+      <script src="{{asser('/js/app.js')}}"></script>
+</body>
+</html>            
