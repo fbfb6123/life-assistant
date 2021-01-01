f53baa421ec44ac46e66078e70569c5afb0c3a62
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-01 21:25:46 +0900

    require('./index.js')

--- resources/js/app.js
+++ resources/js/app.js
@@ -12,6 +12,5 @@ require('./bootstrap');
  * or customize the JavaScript scaffolding to fit your unique needs.
  */
 
-require('./components/Example');
-require('./components/MyComponent');
-require('./components/StbComponent');
+
+require('./index.js');
