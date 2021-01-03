f20c5b462fe5b2efdf65aaefaaf4d2bb7b5077a0
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-03 22:08:16 +0900

    Expense(費用)Model/migration生成

--- database/migrations/2021_01_03_130632_create_expenses_table.php
+++ database/migrations/2021_01_03_130632_create_expenses_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateSpendingsTable extends Migration
+class CreateExpensesTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateSpendingsTable extends Migration
      */
     public function up()
     {
-        Schema::create('spendings', function (Blueprint $table) {
+        Schema::create('expenses', function (Blueprint $table) {
             $table->id();
             $table->string('text')->comment('名前');
             $table->integer('amount')->nullable()->comment('金額');
@@ -29,6 +29,6 @@ class CreateSpendingsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('spendings');
+        Schema::dropIfExists('expenses');
     }
 }
