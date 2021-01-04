4cc8918c7b83dc7f3fe766ba412aa672df805542
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-04 22:23:58 +0900

    Update Home.js

--- resources/js/components/Home.js
+++ resources/js/components/Home.js
@@ -1,1 +1,8 @@
 //親コンポーネントから子コンポーネントに渡す
+
+const [inputText, setInputText] = useState("");
+  const [inputAmount, setInputAmount] = useState(0);
+  const [incomeItems, setIncomeItems] = useState([]);
+  const [expenseItems, setExpenseItems] = useState([]);
+  const [type, setType] = useState("inc");
+  const [date, setDate] = useState(new Date());
