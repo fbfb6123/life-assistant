03dec655fc1a8465498545ac8bd6287d503a1da8
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-07 23:17:00 +0900

    style記述

--- resources/js/components/Item.js
+++ resources/js/components/Item.js
@@ -3,7 +3,31 @@ import { connect } from 'react-redux';
 
 class Item extends Component {
 
-  
+  th = {
+    fontSize:"14pt",
+    backgroundColor:"blue",
+    color:"white",
+    padding:"5px 10px",
+    width:"50px"
+  }
+
+  td = {
+    fontSize:"14pt",
+    backgroundColor:"white",
+    color:"darkblue",
+    padding:"5px 10px",
+    border:"1px solid lightblue",
+    minWidth:"300px"
+  }
+
+  data = {
+    fontSize:"14pt",
+    backgroundColor:"white",
+    color:"darkblue",
+    padding:"5px 10px",
+    border:"1px solid lightblue",
+    width:"80px"
+  }
 }
 
 export default Item;
