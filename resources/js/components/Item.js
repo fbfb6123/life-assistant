777acbfbde87224419a3ef21bf227d35571aeebf
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-07 23:23:52 +0900

    Update Item.js

--- resources/js/components/Item.js
+++ resources/js/components/Item.js
@@ -20,7 +20,7 @@ class Item extends Component {
     minWidth:"300px"
   }
 
-  data = {
+  date = {
     fontSize:"14pt",
     backgroundColor:"white",
     color:"darkblue",
@@ -28,6 +28,17 @@ class Item extends Component {
     border:"1px solid lightblue",
     width:"80px"
   }
+
+  render(){
+    let d = this.props.value.created;
+    let f = d.getHours() + ':' + d.getMinutes() + ':' + d.getSeconds();
+    return (
+    <tr><th style={this.th}>No, {this.props.index}</th>
+      <td style={this.td}>{this.props.value.message}</td>
+      <td style={this.date}>{f}</td>
+      </tr>
+    );
+  }
 }
 
-export default Item;
+export default conect() (Item);
