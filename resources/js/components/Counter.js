95dd7f9fe55a17d200b3c74ad10f96c390ae7917
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-07 23:11:11 +0900

    モードによる表示の分岐。Counterをコネクト

--- resources/js/components/Counter.js
+++ resources/js/components/Counter.js
@@ -4,7 +4,39 @@ import Item from './Item';
 
 class Counter extends Component {
 
+  render(){
+    let data;
+    let n = 0;
 
+    switch (this.props.mode){
+      case 'default':
+        data = this.props.data.map((value)=>(
+          <Item key={value.message} value={value} index={n++} />
+        ));
+        breack;
+
+      case 'find': 
+      data = this.props.fdata.map((value)=>(
+        <Item key={value.message} value={value} index={n++} />
+      ));
+      breack;
+
+      case 'delete':
+        data = this.props.data.map((value)=>(
+          <Item key={value.message} value={value} index={n++} />
+        ));
+        breack;
+
+        default:
+          data = this.props.data.map((value)=>(
+            <Item key={value.message} value={value} index={n++} />
+          ));
+    }
+
+    return (
+      <table><tbody>{data}</tbody></table>
+    );
+  }
 }
 
-export default Counter;
+export default connect((state)=>state) (Counter);
