c0989866cea043b5f282ae0f74b85cf08911a1ed
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-08 23:28:24 +0900

    import記述

--- resources/js/expense/DelForm.js
+++ resources/js/expense/DelForm.js
@@ -1,1 +1,8 @@
 //削除処理
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { delExpense } from '../components/Store';
+
+class DelForm extends Component {
+}
+export default connect((state)=>state)(DelForm);
