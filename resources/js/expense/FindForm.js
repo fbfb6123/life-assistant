84f93833b2dcb9e99af5c20a34fc327db99edbde
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-08 23:28:39 +0900

    import記述

--- resources/js/expense/FindForm.js
+++ resources/js/expense/FindForm.js
@@ -1,1 +1,8 @@
 //取得処理（検索）
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { findExpense } from '../components/Store';
+
+class FindForm extends Component {
+}
+export default connect((state)=>state)(FindForm);
