ad396979702a61cb5ea7f753f6393c43d27b6e9f
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-09 21:53:29 +0900

    Update DelForm.js

--- resources/js/expense/DelForm.js
+++ resources/js/expense/DelForm.js
@@ -1,8 +1,28 @@
 //削除処理
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-import { delExpense } from '../components/Store';
+import { deleteExpense } from '../components/Store';
 
 class DelForm extends Component {
+  input = {
+    fontSize:"12pt",
+    color:"#006",
+    padding:"1px",
+    margin:"5px 0px"
+  }
+  btn = {
+    fontSize:"10pt",
+    color:"#006",
+    padding:"2px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      number:0
+    }
+    this.doChange = this.doChange.bind(this);
+    this.doAction = this.doAction.bind(this);
+  }
 }
 export default connect((state)=>state)(DelForm);
