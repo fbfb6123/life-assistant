332c2ee23c16262b875586261daae722e4196c4c
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-09 22:17:05 +0900

    Update FindForm.js

--- resources/js/expense/FindForm.js
+++ resources/js/expense/FindForm.js
@@ -4,5 +4,36 @@ import { connect } from 'react-redux';
 import { findExpense } from '../components/Store';
 
 class FindForm extends Component {
+  input = {
+    fontSize:"14pt",
+    color:"#006",
+    padding:"0px",
+  }
+  btn = {
+    fontSize:"12pt",
+    color:"#006",
+    padding:"1px 10px"
+  }
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      find:''
+    }
+    this.doChange = this.doChange.bind(this);
+    this.doAction = this.doAction.bind(this);
+  }
+
+  doChange(e) {
+    this.setState({
+      find: e.target.value
+    });
+  }
+
+  doAction(e) {
+    e.preventDefault();
+    let action = findExpense(this.state.number);
+    this.props.dispatch(action); //Storeのレデューサーにactionが送られる
+  }
 }
 export default connect((state)=>state)(FindForm);
