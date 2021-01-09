0354580b7216785f98dd86dcfa0defb1cee3785b
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-09 22:04:59 +0900

    Update DelForm.js

--- resources/js/expense/DelForm.js
+++ resources/js/expense/DelForm.js
@@ -24,5 +24,35 @@ class DelForm extends Component {
     this.doChange = this.doChange.bind(this);
     this.doAction = this.doAction.bind(this);
   }
+
+  doChange(e) {
+    this.setState({
+      number: e.target.value
+    });
+  }
+
+  doAction(e) {
+    e.preventDefault();
+    let action = deleteMemo(this.state.number);
+    this.props.dispatch(action); //Storeのレデューサーにactionが送られる
+    this.setState({
+      number: 0
+    });
+  }
+
+  render(){
+    let n = 0;
+    let items = this.props.data.map((value)=>(
+    <option key={n} value={n++}>{value.message.substring(0,10)}</option>
+    ));
+    return (
+      <div>
+        <form onSubmit={this.doAction}>
+          <select onChange={this.doChange}
+          defaultValue="-1" style={this.btn} value="Del"/>
+        </form>
+      </div>
+    );
+  }
 }
 export default connect((state)=>state)(DelForm);
