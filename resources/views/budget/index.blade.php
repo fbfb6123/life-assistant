ee227911614f4aede18b0a694c794d333c030b1f
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-12 05:22:24 +0900

    index.blade修正

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -11,13 +11,13 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body style="padding:10px;">
-      <h1>Hello</h1>
+      <!-- <h1>Hello</h1>
       <p>{{$msg}}</p>
       <ol>
     @foreach($person as $item)
         <li>{{$item->name}} [{{$item->email}},{{$item->age}}]</li>
     @endforeach
-      </ol>
+      </ol> -->
       <div id="mycomponent"></div>
 
       <script src="{{asset('/js/app.js')}}"></script>
