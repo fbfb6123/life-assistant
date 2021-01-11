16f50970f6f09076f07d1826c14f901c655a231f
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-11 20:36:43 +0900

    Update index.js

--- resources/js/index.js
+++ resources/js/index.js
@@ -3,10 +3,20 @@ import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
 // import TodoApp from './components/TodoApp';
 import App from './components/App';
-import ExpenceStore from './components/Store';
 import { PersistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';//storageに保存
 import { PersistGate } from 'redux-persist/integration/react';
+import ExpenceStore, { expenceRedecer} from './components/Store';
+
+// Redux Persistの設定
+const persistConfig = {
+  key: 'memo',
+  storage,
+};
+
+//パーシスとレデューサーの作成
+const persistedReducer = persistReducer(persistConfig, expenceRedecer);
+
 
 
 //表示をレンダリング
