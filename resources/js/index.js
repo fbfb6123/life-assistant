574905563e03e3910b70ebef530ec81377757b1c
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-11 21:38:06 +0900

    Reduser import 動作確認済み

--- resources/js/index.js
+++ resources/js/index.js
@@ -1,31 +1,39 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
 import { Provider } from 'react-redux';
 // import TodoApp from './components/TodoApp';
 import App from './components/App';
-import { PersistStore, persistReducer } from 'redux-persist';
+import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';//storageに保存
 import { PersistGate } from 'redux-persist/integration/react';
-import ExpenceStore, { expenceRedecer} from './components/Store';
+import ExpenceStore, { homeReducer} from './components/Store';
 
 // Redux Persistの設定
 const persistConfig = {
-  key: 'memo',
+  key: 'expence',
   storage,
 };
 
 //パーシスとレデューサーの作成
-const persistedReducer = persistReducer(persistConfig, expenceRedecer);
+const persistedReducer = persistReducer(persistConfig, homeReducer);
 
-
+//ストア、パーシスターの作成
+let store = createStore(persistedReducer);
+let pstore = persistStore(store);
 
 //表示をレンダリング
 ReactDOM.render(
-  <Provider store={ExpenceStore}>
-    <App />
+  <Provider store={store}>
+    <PersistGate loading={<p>loading...</p>}
+                 persistor={pstore}>
+           <App />
+    </PersistGate>
   </Provider>,
   document.getElementById('mycomponent')
 )
+
+export default pstore;
 
 // const store = createStore();
 
