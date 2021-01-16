024afbcf848e9aa0dff389a0b65615fd421e7820
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-17 08:51:45 +0900

    Create Form.js

---
+++ resources/js/components/employee/Form.js
@@ -0,0 +1,77 @@
+import React, { useEffect, useState } from 'react';
+import employeeServices from "./Employee";
+
+function Form(){
+
+  
+  const [ name, setName ] = useState(null);
+  const [ email, setEmail ] = useState(null);
+  const [ age, setAge ] = useState(null);
+
+  // const [ ListRol, setListRol] = useState([]);
+
+  // useEfect(()=>{
+
+  //   async function featchDataRol(){
+  //     const res = await employeeServices.list();
+  //     console.log(res.data);
+  //     setListRol(res.data)
+  //   }
+
+  //   featchDataRol();
+  // },[])
+
+  const saveEmployee = async () => {
+
+    const data = {
+      name, email, age
+    }
+
+    const res = await employeeServices.save(data);
+
+    // if(res.success) {
+    //   alert(res,message)
+    // } else {
+    //   alert(res,message)
+    // }
+  }
+
+  return(
+    <div>
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="firstName">Name</label>
+          <input type="text" className="form-control" placeholder="Name"
+            onChange={(event)=>setName(event.target.value)}/>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="email">Email</label>
+          <input type="email" className="form-control" placeholder="you@example.com"
+          onChange={(event)=>setEmail(event.target.value)}/>
+        </div>
+      </div>
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <label htmlFor="address">Age</label>
+          <input type="text" className="form-control" placeholder="1234 Main St"
+          onChange={(event)=>setAge(event.target.value)}/>
+        </div>
+      </div>
+
+
+      <div className="row">
+        <div className="col-md-6 mb-3">
+          <button
+          className="btn btn-primary btn-block" type="submit"
+          onClick={()=>saveEmployee()}>Save</button>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Form
