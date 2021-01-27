f398214d45d4203a3c7c64b247836a52ef086230
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-01-28 07:14:50 +0900

    update index.blade.php

--- resources/views/budget/index.blade.php
+++ resources/views/budget/index.blade.php
@@ -11,6 +11,7 @@
 <meta name="csrf-token" content="{{ csrf_token() }}">
 </head>
 <body class=container>
+  <div class="header"></div>
   <div id="mycomponent"></div>
 
 <script src="{{asset('/js/app.js')}}"></script>
