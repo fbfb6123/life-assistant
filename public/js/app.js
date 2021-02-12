de65674713835772d8af112d577074942c1752f5
fbfb6123 <60612010+fbfb6123@users.noreply.github.com>
2021-02-12 18:45:24 +0900

    Update app.js

--- public/js/app.js
+++ public/js/app.js
@@ -97916,10 +97916,7 @@ var Income = function Income(_ref) {
         className: "income-item-text"
       }, income.text), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
         className: "income-item-amount"
-      }, "+", Number(income.amount).toLocaleString(), "\u5186"), /*#__PURE__*/react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("button", {
-        className: "delete-btn",
-        onClick: deleteHandler
-      }, "\xD7"));
+      }, "+", Number(income.amount).toLocaleString(), "\u5186"));
     }))));
   };
 
